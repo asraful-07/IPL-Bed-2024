eaa996c2c4bab0071f2e4cdf5874662c5f016c3c
asraful-07 <asraful0508@gmail.com>
2024-10-27 11:46:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center items-center bg-red-100 p-6 text-3xl font-medium m-10">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
